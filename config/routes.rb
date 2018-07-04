a967b8d11355f4eabdb0f2152e6148693e521d48
Bruno <seletonurb@gmail.com>
2018-07-04 12:55:46 -0300

    Add missing category resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-    resources :posts
+  resources :posts
+  resources :categories
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
