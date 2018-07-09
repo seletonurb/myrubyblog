401b325f1f6c1de6cfa4284d59695403dd2e7c1b
Bruno <seletonurb@gmail.com>
2018-07-09 10:06:52 -0300

    Applying categories to posts

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,17 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
-
   # GET /categories
-  # GET /categories.json
   def index
     @categories = Category.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @categories }
-    end
   end
 
   def category_params
     params.require(:category).permit(:name)
   end
   # GET /categories/1
-  # GET /categories/1.json
   def show
     @category = Category.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @category }
-    end
+    @title = @category.name
+    @posts = @category.posts
   end
 
 end
